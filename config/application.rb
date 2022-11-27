bd98423c34531fa01a878bb8d079ea2e6fc8aa98
Daniel Täsch <accounts@datae.de>
2022-11-27 14:09:46 +0100

    setup devise

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Testanwendung
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.navigational_formats = ['/', :html, :turbo_stream]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+unless User.all.pluck(:email).include?("mail@example.com")
+  User.create({
+    email: "mail@example.com",
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf"
+  })
+end
